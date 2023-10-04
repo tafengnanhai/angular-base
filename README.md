f95bbf03298dec88ec273b6d8205f3a1b2b8df0f
tafengnanhai <tafengnanhai@foxmail.com>
2023-10-04 21:23:19 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-base
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/jmahuh)
---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product, products } from '../products';
+import { CartService } from '../cart.service';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css'],
+})
+export class ProductDetailsComponent implements OnInit {
+  product: Product | undefined;
+  constructor(private route: ActivatedRoute) {}
+  ngOnInit() {
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
+    this.product = products.find(
+      (product) => product.id === productIdFromRoute
+    );
+  }
+}
