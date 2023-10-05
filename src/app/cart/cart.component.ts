d3e0859610d66ea8a05874881f17afc2bf5d3264
tafengnanhai <tafengnanhai@foxmail.com>
2023-10-05 11:44:06 +0800

    2 files modified

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -2,15 +2,29 @@ import { Component, inject } from '@angular/core';
 import { Product } from '../products';
 import { CartService } from '../cart.service';
 import { NgFor, CurrencyPipe } from '@angular/common';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.scss'],
   standalone: true,
-  imports: [NgFor, CurrencyPipe],
+  imports: [NgFor, CurrencyPipe, ReactiveFormsModule],
 })
 export class CartComponent {
   cartService = inject(CartService);
   items: Product[] = this.cartService.getItems();
+  formBuilder = inject(FormBuilder);
+  formData = this.formBuilder.group({
+    name: '',
+    address: '',
+  });
+
+  onSubmit() {
+    this.cartService.clearCart();
+    window.alert(
+      'Success:' + this.formData.value.name + ',' + this.formData.value.address
+    );
+    this.formData.reset();
+  }
 }
